0d97c1f8d747c689e8b49a50fcacbdf96295bc77
Hayden Swift <19swifth@queenschester.com>
2024-09-30 12:28:29 +0100

    15 point star

--- shapes.py
+++ shapes.py
@@ -1,6 +1,10 @@
 import turtle as t
-t.pencolor('blue')
-t.pendown()
-for i in range(5):
-	t.forward(100)
-	t.right(144)
+DEGREES_IN_A_CIRCLE = 360
+DEGREES_IN_A_LINE = 180
+def star(points,size):
+    if points % 2 == 1:
+        angle = int(DEGREES_IN_A_LINE-(DEGREES_IN_A_LINE/points))
+        for i in range(points):
+            t.forward(size)
+            t.right(angle)
+star(15,300)
